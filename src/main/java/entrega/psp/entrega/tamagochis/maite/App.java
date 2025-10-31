080742130831bf8ea5973fdec93d7fdc5c3babc9
Maite <maitesolam@gmail.com>
2025-10-31 08:48:06 +0100

    Esqueleto proyecto entrega Tamagochis

---
+++ src/main/java/entrega/psp/entrega/tamagochis/maite/App.java
@@ -0,0 +1,13 @@
+package entrega.psp.entrega.tamagochis.maite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
