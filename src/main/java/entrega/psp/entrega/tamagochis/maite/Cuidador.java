d4d33ced377caa329c486b63ff3991953e2478bf
Maite <maitesolam@gmail.com>
2025-11-03 12:44:38 +0100

    Implementado menú funcional del cuidador: los tamagotchis pueden alimentarse, jugar, limpiarse, morir y mostrar su estado.

--- src/main/java/entrega/psp/entrega/tamagochis/maite/Cuidador.java
+++ src/main/java/entrega/psp/entrega/tamagochis/maite/Cuidador.java
@@ -1,19 +1,163 @@
 package entrega.psp.entrega.tamagochis.maite;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Cuidador {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int PARTICIPANTES = 5;
 
-		//Programa principal. Se encarga de crear y gestionar los Tamagochis
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// Programa principal. Se encarga de crear y gestionar los Tamagochis
+		ArrayList<Tamagochi> listaTamagochis = new ArrayList<>();
+		// 1 - Crear Tamagochis
+		listaTamagochis = crearTamagochis();
+
+		// 2- los lanzamos en su propio hilo
+		for (Tamagochi t : listaTamagochis) {
+			Thread hilo = new Thread(t);
+			hilo.start();
+			System.out.println("Los tamagochis han empezado a vivir");
+
+		}
+
+		// 2 -Ordenes del tamagochi
+		menu(scan, listaTamagochis);
+
+		// 3- Comunicación entre Tamagochis
+	}
+
+	// Creamos los tamagochis
+	public static ArrayList<Tamagochi> crearTamagochis() {
+		Random rnd = new Random();
+		ArrayList<Tamagochi> lista = new ArrayList<>();
+		Tamagochi t;
+		for (int i = 0; i < PARTICIPANTES; i++) {
+
+			String nombre = "t" + i;
+			long ritmoComida = rnd.nextLong(10000, 80000);
+
+			t = new Tamagochi(nombre, ritmoComida);
+			lista.add(t);
+		}
+		return lista;
+
+	}
+
+	public static void menu(Scanner scan, ArrayList<Tamagochi> listaTamagochis) {
+
+		String opcion = "";
+		do {
+
+			System.out.println("MENÚ TAMAGOCHI");
+			System.out.println("\n===== MENÚ TAMAGOCHI =====");
+			System.out.println("1. Alimentar Tamagotchi");
+			System.out.println("2. Jugar con Tamagotchi");
+			System.out.println("3. Limpiar Tamagotchi");
+			System.out.println("4. Ver estado de todos");
+			System.out.println("5. Matar un Tamagotchi");
+
 		
+
+			System.out.println("6. Salir");
+			System.out.print("Elige una opción: ");
+			opcion = scan.nextLine();
+			switch (opcion) {
+
+			case "1":
+				alimentarse(scan, listaTamagochis);
+				break;
+			case "2":
+				jugar(scan, listaTamagochis);
+				break;
+			case "3":
+				limpiar(scan, listaTamagochis);
+				break;
+			case "4":
+				consultarEstado(scan, listaTamagochis);
+				break;
+			case "5":
+			    matar(scan, listaTamagochis);
+			    break;
+			case "6":
+				System.out.println("Salir");
+				break;
+			}
+
+		} while (!opcion.equals("6"));
+	}
+
+	public static void alimentarse(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
+
+		Tamagochi t = seleccionar(scan, listaTamagochi);
+
+		if (t != null) {
+
+			t.alimentar();
+		}
+
+	}
+
+	public static void jugar(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
+
+		Tamagochi t = seleccionar(scan, listaTamagochi);
+
+		if (t != null) {
+
+			t.jugar();
+		}
+
+	}
+
+	public static void limpiar(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
+
+		Tamagochi t = seleccionar(scan, listaTamagochi);
+
+		if (t != null) {
+
+			t.limpiar();
+		}
+	}
+
+	public static void matar(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
+
+		String motivo = "El cuidador lo ha matado";
+		Tamagochi t = seleccionar(scan, listaTamagochi);
+
+		if (t != null) {
+
+			t.morir(motivo);
+		}
+	}
+
+	public static void consultarEstado(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
 		
-		//1 - Crear Tamagochis
-		
-		
-		//2 -Ordenes del tamagochi
-		
-		//3- Comunicación entre Tamagochis
+		System.out.println("\n===== ESTADO DE TAMAGOTCHIS =====");
+	    for (Tamagochi t : listaTamagochi) {
+	        System.out.printf(
+	            "%s → Vivo: %b | Ocupado: %b | Suciedad: %d%n",
+	            t.getNombre(), t.isEstaVivo(), t.isOcupado(), t.getSuciedad()
+	        );
+	    }
+	    System.out.println("=================================\n");
+
+	}
+
+	public static Tamagochi seleccionar(Scanner scan, ArrayList<Tamagochi> listaTamagochi) {
+
+		System.out.println("Elige un tamagochi");
+		for (int i = 0; i < listaTamagochi.size(); i++) {
+			Tamagochi t = listaTamagochi.get(i);
+			System.out.printf("%d. %s (vivo: %b, ocupado: %b)%n", i + 1, t.getNombre(), t.isEstaVivo(), t.isOcupado());
+		}
+		int indice = Integer.parseInt(scan.nextLine()) - 1;
+		if (indice >= 0 && indice < listaTamagochi.size()) {
+			return listaTamagochi.get(indice);
+		}
+		return null;
+
 	}
 
 }
--- src/main/java/entrega/psp/entrega/tamagochis/maite/Tamagochi.java
+++ src/main/java/entrega/psp/entrega/tamagochis/maite/Tamagochi.java
@@ -1,21 +1,26 @@
 package entrega.psp.entrega.tamagochis.maite;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tamagochi implements Runnable {
 
+	Scanner scan = new Scanner(System.in);
 
-	private int ritmoComida; // Controlo el tiempo que tarda en comer
+	private long ritmoComida; // Controlo el tiempo que tarda en comer
 	private int suciedad = 0;
 	private boolean estaVivo = true;
 	private long tiempoInicio;
 	private boolean ocupado = false;
-	
-	//getters
+
+	// getters
 	private String nombre;
+
 	public String getNombre() {
 		return nombre;
 	}
 
-	public int getRitmoComida() {
+	public long getRitmoComida() {
 		return ritmoComida;
 	}
 
@@ -35,14 +40,11 @@ public class Tamagochi implements Runnable {
 		return ocupado;
 	}
 
-	
-
-	public Tamagochi(String nombre, int ritmoComida) {
+	public Tamagochi(String nombre, long ritmoComida) {
 		super();
 		this.nombre = nombre;
 		this.ritmoComida = ritmoComida;
 		this.tiempoInicio = System.currentTimeMillis(); // Para controlar el tiempo de vida.
-
 	}
 
 	@Override
@@ -53,7 +55,8 @@ public class Tamagochi implements Runnable {
 			// Controlas la suciedad
 
 			try {
-				Thread.sleep(2000);
+			
+				Thread.sleep(20000);
 				suciedad += 1;
 				if (suciedad == 5) {
 					System.out.println("Estoy empezando a estar muy sucio...");
@@ -65,7 +68,7 @@ public class Tamagochi implements Runnable {
 				}
 
 				if (System.currentTimeMillis() - tiempoInicio > 300000) {
-					// 30000 son 5 minutos en milisegundos
+					// 300.000 ms son 5 minutos
 					morir("ha muerto de viejo");
 				}
 
@@ -78,15 +81,74 @@ public class Tamagochi implements Runnable {
 
 	}
 
-	
-	
-	public synchronized void morir(String motivo) {
+	public void morir(String motivo) {
 
 		if (!estaVivo)
 			return; // Si ya no está no se puede matar
 		estaVivo = false;
+		ocupado = false;
 		System.out.println(nombre + " " + motivo);
+	}
 
+	public void alimentar() {
+
+		if (ocupado || !estaVivo)
+			return;
+		ocupado = true;
+
+		try {
+			System.out.println("Empiezo a comer");
+			Thread.sleep(ritmoComida);
+			System.out.println("He terminado de comer");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			ocupado = false;
+		}
+
+	}
+
+	public void jugar() {
+
+		if (ocupado || !estaVivo)
+			return;
+		ocupado = true;
+		Random rnd = new Random();
+		int num1 = rnd.nextInt(0, 10);
+		int num2 = rnd.nextInt(0, 10);
+		int resultado = num1 + num2;
+		int result = 0;
+		System.out.println("VAMOS A JUGAR A SUMAS");
+		do {
+			System.out.println(num1 + " + " + num2 + "= ");
+			String resultadoStr = scan.nextLine();
+			result = Integer.parseInt(resultadoStr);
+
+		} while (resultado != result);
+
+		System.out.println("¡Acertaste!");
+
+	}
+
+	public void limpiar() {
+
+		if (ocupado || !estaVivo)
+			return;
+		ocupado = true;
+
+		try {
+			System.out.println("Empiezo a limpiarme");
+			Thread.sleep(5000);
+
+			suciedad = 0;
+			System.out.println("Me he limpiado");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			ocupado = false;
+		}
 	}
 
 }
